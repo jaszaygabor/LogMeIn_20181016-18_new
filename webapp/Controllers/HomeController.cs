f46c0e9fabc00ac72bdab3af76be26fef50b73a3
jaszaygabor <34000521+jaszaygabor@users.noreply.github.com>
2018-10-16 16:33:21 +0200

    LogMeIn_VS

---
+++ webapp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webapp.Models;
+
+namespace webapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult UserDetails()
+        {
+            var bob = new User();
+            bob.Name = "Bob";
+            bob.BirthDate = new DateTime(2000, 1, 1);
+            return View(bob);
+        }
+        public ActionResult halozat()
+        {
+            return View();
+        }
+    }
+}
---
+++ webapp/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webapp.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
